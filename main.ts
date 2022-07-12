d271a1960894b59005ffe6b943764fa94dfd2b15
NickCheung33 <107850266+NickCheung33@users.noreply.github.com>
2022-07-12 14:45:48 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -17,7 +17,13 @@ MakerCloud_KOI.registerTopicMessageHandler("BMTOR6T1", function (receivedMessage
     }
 })
 koi.koi_onrecttrack(function (x, y, w, h) {
-	
+    if (x < 120) {
+        powerbrick.MotorRun(powerbrick.Motors.M1, 0)
+    } else {
+        if (x > 120) {
+        	
+        }
+    }
 })
 koi.koi_init_pw(koi.SerialPorts.PORT2)
 koi.koi_lcd_direction(koi.LcdDirection.Front)
@@ -25,7 +31,10 @@ koi.koi_join_ap("ssid", "password")
 basic.pause(1000)
 MakerCloud_KOI.connectMakerCloudMQTT()
 basic.pause(1000)
-MakerCloud_KOI.subscribeTopic("topic")
+MakerCloud_KOI.subscribeTopic("ZBOP3RO2")
+MakerCloud_KOI.subscribeTopic("BMTOR6T1")
+MakerCloud_KOI.subscribeTopic("BMTOR6T1")
+MakerCloud_KOI.subscribeTopic("BMTOR6T1")
 basic.pause(1000)
 basic.forever(function () {
 	
