ed858d0b405ea4821d18f4809c1a9455e750b89e
NickCheung33 <107850266+NickCheung33@users.noreply.github.com>
2022-07-12 15:42:21 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,16 @@
-MakerCloud.on_wifi_disconnected(function () {
-    koi.koi_cloud_tts("wifi disconnected")
-})
-MakerCloud.on_wifi_connected(function () {
-    koi.koi_cloud_tts("wifi connected")
-})
 MakerCloud_KOI.registerTopicMessageHandler("BMTOR6T1", function (receivedMessage) {
+    if (receivedMessage == "trace") {
+        koi.koi_cloud_tts("start tracing")
+        while (false) {
+            koi.koi_track_rect(6000)
+        }
+    } else {
+        if (receivedMessage == "stop") {
+            koi.koi_stop_kpu()
+        }
+    }
+})
+MakerCloud_KOI.registerTopicMessageHandler("BVKZV6T5", function (receivedMessage) {
     if (receivedMessage == "trace") {
         koi.koi_cloud_tts("start tracing")
         while (false) {
@@ -16,26 +22,37 @@ MakerCloud_KOI.registerTopicMessageHandler("BMTOR6T1", function (receivedMessage
         }
     }
 })
-koi.koi_onrecttrack(function (x, y, w, h) {
-    if (x < 120) {
-        powerbrick.MotorRun(powerbrick.Motors.M1, 0)
-    } else {
-        if (x > 120) {
-        	
+function autofollow () {
+    while (true) {
+        basic.pause(50)
+        left_sonar = powerbrick.Ultrasonic(powerbrick.Ports.PORT1)
+        right_sonar = powerbrick.Ultrasonic(powerbrick.Ports.PORT1)
+        if (right_sonar > 3 && right_sonar < 10) {
+            powerbrick.Servo360(powerbrick.Servos.S1, 255)
+            powerbrick.Servo360(powerbrick.Servos.S2, 255)
+        } else {
+            powerbrick.Servo360(powerbrick.Servos.S1, 0)
+            powerbrick.Servo360(powerbrick.Servos.S2, 0)
+        }
+        if (left_sonar > 3 && left_sonar < 10) {
+            powerbrick.Servo360(powerbrick.Servos.S7, 255)
+            powerbrick.Servo360(powerbrick.Servos.S8, 255)
+        } else {
+            powerbrick.Servo360(powerbrick.Servos.S7, 0)
+            powerbrick.Servo360(powerbrick.Servos.S8, 0)
         }
     }
-})
+}
+let right_sonar = 0
+let left_sonar = 0
+autofollow()
 koi.koi_init_pw(koi.SerialPorts.PORT2)
 koi.koi_lcd_direction(koi.LcdDirection.Front)
 koi.koi_join_ap("ssid", "password")
 basic.pause(1000)
 MakerCloud_KOI.connectMakerCloudMQTT()
 basic.pause(1000)
-MakerCloud_KOI.subscribeTopic("ZBOP3RO2")
 MakerCloud_KOI.subscribeTopic("BMTOR6T1")
 MakerCloud_KOI.subscribeTopic("NBGLJS62")
 MakerCloud_KOI.subscribeTopic("BVKZV6T5")
 basic.pause(1000)
-basic.forever(function () {
-	
-})
