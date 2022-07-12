54544a4dd15c6e4234369021c0e20fb9ab9e28db
NickCheung33 <107850266+NickCheung33@users.noreply.github.com>
2022-07-12 11:39:31 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
 koi.koi_onrecttrack(function (x, y, w, h) {
     koi.koi_track_rect(6000)
 })
+MakerCloud.configRxTxPwbrick(MakerCloud.SerialPorts.PORT1)
+MakerCloud.setupWifi("", "")
+MakerCloud.connectMakerCloudMQTT()
 koi.koi_init_pw(koi.SerialPorts.PORT1)
+koi.koi_cloud_tts("Welcome to use HUC!")
 basic.forever(function () {
 	
 })
