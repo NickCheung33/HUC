35df68580e77019a0ffbd905beb4fbd7a79203a0
NickCheung33 <107850266+NickCheung33@users.noreply.github.com>
2022-07-12 13:49:35 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,23 @@
+MakerCloud.on_wifi_disconnected(function () {
+    koi.koi_cloud_tts("wifi disconnected")
+})
+MakerCloud.on_wifi_connected(function () {
+    koi.koi_cloud_tts("wifi connected")
+})
 koi.koi_onrecttrack(function (x, y, w, h) {
     koi.koi_track_rect(6000)
 })
-MakerCloud.configRxTxPwbrick(MakerCloud.SerialPorts.PORT1)
-MakerCloud.setupWifi("", "")
-MakerCloud.connectMakerCloudMQTT()
-koi.koi_init_pw(koi.SerialPorts.PORT1)
-koi.koi_cloud_tts("Welcome to use HUC!")
+MakerCloud_KOI.registerTopicMessageHandler("topic", function (receivedMessage) {
+	
+})
+koi.koi_init_pw(koi.SerialPorts.PORT2)
+koi.koi_lcd_direction(koi.LcdDirection.Front)
+koi.koi_join_ap("ssid", "password")
+basic.pause(1000)
+MakerCloud_KOI.connectMakerCloudMQTT()
+basic.pause(1000)
+MakerCloud_KOI.subscribeTopic("topic")
+basic.pause(1000)
 basic.forever(function () {
 	
 })
