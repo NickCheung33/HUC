6378743aec12311b04f8717b817d214b9ebbb942
NickCheung33 <107850266+NickCheung33@users.noreply.github.com>
2022-07-14 23:28:53 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,10 @@
-let left_sonar = 0
-let right_sonar = 0
+MakerCloud_KOI.registerTopicMessageHandler("BMTOR6T1", function (receivedMessage) {
+    if (receivedMessage == "follow") {
+        flag = !(flag)
+    } else if (receivedMessage == "stop") {
+        flag = flag
+    }
+})
 function autofollow () {
     left_sonar = powerbrick.Ultrasonic(powerbrick.Ports.PORT3)
     right_sonar = powerbrick.Ultrasonic(powerbrick.Ports.PORT4)
@@ -17,12 +22,16 @@ function autofollow () {
         powerbrick.MotorRun(powerbrick.Motors.M2, 0)
     }
 }
-input.onButtonPressed(Button.A, function () {
-    powerbrick.Servo(powerbrick.Servos.S3, 20)
-})
-input.onButtonPressed(Button.B, function () {
-    powerbrick.Servo(powerbrick.Servos.S3, -20)
-})
+let right_sonar = 0
+let left_sonar = 0
+let flag = false
+koi.koi_init_pw(koi.SerialPorts.PORT2)
+koi.koi_join_ap("ssid", "password")
+flag = false
+MakerCloud_KOI.connectMakerCloudMQTT()
+MakerCloud_KOI.subscribeTopic("BMTOR6T1")
 basic.forever(function () {
-    autofollow()
+    if (flag) {
+        autofollow()
+    }
 })
